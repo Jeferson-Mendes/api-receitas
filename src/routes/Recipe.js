542b9d5f3f81d3ef8f04e1690c663ed92ad850a5
jeferson-mendes <jefersonmendes3c@gmail.com>
2021-12-19 20:54:35 -0300

    chore: improve recipe routes

--- src/routes/Recipe.js
+++ src/routes/Recipe.js
@@ -8,12 +8,13 @@ const RecipeController =  require('../controllers/RecipeController');
 
 RecipeRouter.get('/', RecipeController.index); // Recipe list
 RecipeRouter.get('/:recipeId', RecipeController.detail);
-RecipeRouter.get('/search', RecipeController.search);
+RecipeRouter.get('/search/search', RecipeController.search);
+RecipeRouter.get('/filter/category', RecipeController.getByCategory);
 
 RecipeRouter.use(authMiddleware) // Check token 
 
-RecipeRouter.post('/recipe/create', upload.single('image'), RecipeController.create);
-RecipeRouter.put('/recipe/update/:recipeId', upload.single('image'), RecipeController.update);
-RecipeRouter.delete('/recipe/delete/:recipe', RecipeController.delete);
+RecipeRouter.post('/create', upload.single('image'), RecipeController.create);
+RecipeRouter.put('/update/:recipeId', upload.single('image'), RecipeController.update);
+RecipeRouter.delete('/delete/:recipe', RecipeController.delete);
 
 module.exports = RecipeRouter;
